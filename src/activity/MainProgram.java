5d0f531a4a9498cc192758082b4542cff178826d
Botti <botti@Bottis-MacBook-Air.local>
2021-03-10 10:29:37 -0800

    I did finish tasks for Matrix Object (MainProgram, SquareMatrix).

--- src/activity/MainProgram.java
+++ src/activity/MainProgram.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 public class MainProgram {
 
     public static SquareMatrix read(String location) throws IOException {
-        File file      = new File(location);
-        Scanner input  = new Scanner(file);
-        int size       = input.nextInt();
+        File file = new File(location);
+        Scanner input = new Scanner(file);
+        int size = input.nextInt();
         int[][] matrix = new int[size][size];
 
         for (int i = 0; i < size; i++) {
@@ -23,16 +23,16 @@ public class MainProgram {
     }
 
     public static void write(String location, StringBuilder builder) throws IOException {
-        File file           = new File(location);
-        PrintWriter writer  = new PrintWriter(file);
+        File file = new File(location);
+        PrintWriter writer = new PrintWriter(file);
 
         writer.println(builder);
         writer.close();
     }
 
     public static void main(String[] args) throws IOException {
-        String location       = "data" + File.separator + "matrix.txt";
-        String otherLocation  = "data" + File.separator + "other_matrix.txt";
+        String location = "data" + File.separator + "matrix.txt";
+        String otherLocation = "data" + File.separator + "other_matrix.txt";
         String outputLocation = "data" + File.separator + "output.txt";
 
         // Create object to append string output of matrix
@@ -40,9 +40,49 @@ public class MainProgram {
 
         // TODO : Create matrices for testing
 
-        // TODO : Test operations of Square Matrices
+        int size = 3;
+        int[][] mtrx = new int[3][3];
 
-        // write to output file
+        // Create matrix
+        SquareMatrix matrix = new SquareMatrix(mtrx);
+        SquareMatrix matrixSize = new SquareMatrix(size);
+
+        // read matrix from file
+        SquareMatrix mainMatrix = read(location);
+        SquareMatrix otherMatrix = read(otherLocation);
+
+
+        // TODO : Test operations of Square Matrices:
+
+        // operations Add Square Matrices
+        SquareMatrix resAddMatrix = mainMatrix.add(otherMatrix);
+        // operations Subtract Square Matrices
+        SquareMatrix resSubtractMatrix = mainMatrix.subtract(otherMatrix);
+        // operations Multiply Square Matrices
+        SquareMatrix resMultiplyMatrix = mainMatrix.multiply(otherMatrix);
+
+        // operations Equals Square Matrices
+        boolean resEqualsMatrixFalse = matrix.equals(mainMatrix);
+        boolean resEqualsMatrixTrue = matrix.equals(matrixSize);
+
+        // operations isDiagonal Square Matrices
+        boolean resMatrixIsDiagonalTrue = mainMatrix.isDiagonal();
+        boolean resMatrixIsDiagonalFalse = otherMatrix.isDiagonal();
+
+        // operations Identity Square Matrices
+        boolean resMatrixIdentityTrue = mainMatrix.Identity();
+        boolean resMatrixIdentityFalse = otherMatrix.Identity();
+
+
+
+        // console log results of operation
+
+        System.out.println("Console log results");
+        System.out.print("Results of add operations " + resAddMatrix);
+
+
+
+        // write to output file:
         write(outputLocation, builder);
 
     }
--- src/activity/SquareMatrix.java
+++ src/activity/SquareMatrix.java
@@ -2,7 +2,98 @@ package activity;
 
 public class SquareMatrix {
 
-    //TODO: Update Class
+    int[][] matrix;
+    int size = 3;
+
+    // Generating two constructors (2D array, size):
+
+    public SquareMatrix(int[][] matrix) {
+        int size = matrix.length;
+        this.matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                this.matrix[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    public SquareMatrix(int size) {
+        this.matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                this.matrix[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    public SquareMatrix add(SquareMatrix other) {
+        SquareMatrix obj = new SquareMatrix(size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                obj.matrix[i][j] = this.matrix[i][j] + other.matrix[i][j];
+            }
+        }
+        return obj;
+    }
+
+
+    public SquareMatrix subtract(SquareMatrix other) {
+        SquareMatrix obj = new SquareMatrix(size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                obj.matrix[i][j] = this.matrix[i][j] - other.matrix[i][j];
+            }
+        }
+        return obj;
+    }
+
+    public SquareMatrix multiply(SquareMatrix other) {
+        SquareMatrix A = this;
+        SquareMatrix B = other;
+        SquareMatrix C = new SquareMatrix(size);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++)
+                    C.matrix[i][j] += (A.matrix[i][k] * B.matrix[k][j]);
+            }
+        }
+        return C;
+    }
+
+    public boolean equals(SquareMatrix obj) {
+        if (obj.matrix.length != this.matrix.length) return false;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (obj.matrix[i][j] != this.matrix[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean isDiagonal() {
+        int[][] matrix = this.matrix;
+        for (int i = 0; i < size; i++) {
+            if (matrix[i][i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean Identity() {
+        int[][] matrix = this.matrix;
+        int tempMajor = matrix[0][0];
+
+        for (int i = 0; i < size; i++) {
+            if (matrix[i][i] != tempMajor) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     @Override
     public String toString() {
